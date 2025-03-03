f6620e610c1622f07057b826783cd0f5853e0675
DavidEstevezzz <david2[A[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[C[C[3~[3~[3~[3evez@gmail.com>
2025-03-03 16:16:42 +0100

    actualizacionMensajes4

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -130,7 +130,7 @@ class MessageController extends Controller
         }
         // Asegúrate de que la ruta sea la correcta. 
         // Por ejemplo, si guardaste el archivo en 'attachments', debería ser 'storage/attachments/'.
-        $filePath = public_path('storage/attatchments' . $message->attachment);
+        $filePath = public_path('storage/' . $message->attachment);
         Log::info("Ruta de archivo adjunto: " . $filePath);
         if (!file_exists($filePath)) {
             Log::error("Archivo no encontrado en el servidor. Ruta: " . $filePath);
